c6e24acffd7e3f1f4cd93c50368f3293dbe21849
Jan Budar <honza.budar@gmail.com>
2019-03-29 15:04:31 +0100

    Vyvoření základní adresářové struktruy, kódování hlavního layoutu.

---
+++ RazorComponentsBlog/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorComponentsBlog.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public int UserId { get; set; }
+    }
+}
